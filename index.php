134cb68a8e8835c09e71c423bc09a27a8d1f3ae1
Johan Rodin <johan.n.rodin@se.ibm.com>
2016-03-11 14:37:40 +0000

    More changes.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<scritp src="http://maps.googleapis.com/maps/api/js"></script>
+	<script src="http://maps.googleapis.com/maps/api/js"></script>
 	
 	<script>
 	function loadStations() {
@@ -19,7 +19,7 @@
 		if (data != null) {
 			var myobj = JSON.parse(data);
 			siteid = myobj[0];
-			document.getElementById('lon').innerHTML = myobj[1];
+			document.getElementById('lon').innerHTML = myobj[1]);
 			document.getElementById('lat').innerHTML = myobj[2];   
 		} else console.log("No data returned");
 	
@@ -28,24 +28,25 @@
 			if (data != null) {
 				mytable = data;
 			} else console.log("No data returned");
-			document.getElementById('tablePrint').innerHTML = mytable;
-			google.maps.event.addDomListener(window, 'load', initialize);
+			document.getElementById('tablePrint').innerHTML = mytable;			
 		});
 	});
 	}
 	
-	function initialize()
+	function initialize(my_lon, my_lat)
 	{
+		console.log(document.getElementById(my_lon));
+		console.log(document.getElementById(my_lat));
+		var mapDiv = document.getElementById('googleMap');
   		var mapProp = {
-    	center: new google.maps.LatLng(document.getElementById('lon'),document.getElementById('lat')),
+    	center: new google.maps.LatLng(my_lon,my_lat),
     	zoom:9,
     	mapTypeId: google.maps.MapTypeId.ROADMAP
   		};
   
-  		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp)
+  		var map = new google.maps.Map(mapDiv,mapProp);
 	}
 
-        </script>
 	</script>
 </head>
 <body>
@@ -71,79 +72,78 @@
 	Type your station name: </label>
 	<p>
 	  <input type = "text" id = "stationText" />
-	  <input type = "button" value = "Click here to get realtime information" onclick = "loadStations()"/>
+	  <input type = "button" value = "Click here to get realtime information" onclick = "loadStations();google.maps.event.addDomListener(document.getElementById('googleMap'), 'load', initialize(document.getElementById('lon'),document.getElementById('lat')));"/>
   </p>
+  <div align="center"id="googleMap" style="width:400px;height:300px;"></div>
   <li class="dropdown  style11">
 	  <div>
-	      <p><img src="img/dg8.gif" name="hr1"><img 
-src="images/dg8.gif" name="hr2"><img 
-src="images/dgc.gif"><img 
-src="images/dg8.gif" name="mn1"><img 
-src="images/dg8.gif" name="mn2"><img 
-src="images/dgc.gif"><img 
-src="images/dg8.gif" name="se1"><img 
-src="images/dg8.gif" name="se2"><img 
-src="images/dgam.gif" name="ampm">
+	      <p><img src="img/dg8.gif" name="hr1">
+	      	<img src="images/dg8.gif" name="hr2">
+	      	<img src="images/dgc.gif">
+	      	<img src="images/dg8.gif" name="mn1">
+	      	<img src="images/dg8.gif" name="mn2">
+	      	<img src="images/dgc.gif">
+	      	<img src="images/dg8.gif" name="se1">
+	      	<img src="images/dg8.gif" name="se2">
+	      	<img src="images/dgam.gif" name="ampm">
            
 
-	        
-         
-<body>
-        <div align="center"id="googleMap" style="width:400px;height:300px;"></div>
+        
       </div>
 	  <script type="text/javascript">
-dg = new Array();
-dg[0]=new Image();dg[0].src="images/dg0.gif";
-dg[1]=new Image();dg[1].src="images/dg1.gif";
-dg[2]=new Image();dg[2].src="images/dg2.gif";
-dg[3]=new Image();dg[3].src="images/dg3.gif";
-dg[4]=new Image();dg[4].src="images/dg4.gif";
-dg[5]=new Image();dg[5].src="images/dg5.gif";
-dg[6]=new Image();dg[6].src="images/dg6.gif";
-dg[7]=new Image();dg[7].src="images/dg7.gif";
-dg[8]=new Image();dg[8].src="images/dg8.gif";
-dg[9]=new Image();dg[9].src="images/dg9.gif";
-dgam=new Image();dgam.src="images/dgam.gif";
-dgpm=new Image();dgpm.src="images/dgpm.gif";
+		dg = new Array();
+		dg[0]=new Image();dg[0].src="images/dg0.gif";
+		dg[1]=new Image();dg[1].src="images/dg1.gif";
+		dg[2]=new Image();dg[2].src="images/dg2.gif";
+		dg[3]=new Image();dg[3].src="images/dg3.gif";
+		dg[4]=new Image();dg[4].src="images/dg4.gif";
+		dg[5]=new Image();dg[5].src="images/dg5.gif";
+		dg[6]=new Image();dg[6].src="images/dg6.gif";
+		dg[7]=new Image();dg[7].src="images/dg7.gif";
+		dg[8]=new Image();dg[8].src="images/dg8.gif";
+		dg[9]=new Image();dg[9].src="images/dg9.gif";
+		dgam=new Image();dgam.src="images/dgam.gif";
+		dgpm=new Image();dgpm.src="images/dgpm.gif";
 
-function dotime(){ 
-	var d=new Date();
-	var hr=d.getHours(),mn=d.getMinutes(),se=d.getSeconds();
+		function dotime(){ 
+			var d=new Date();
+			var hr=d.getHours(),mn=d.getMinutes(),se=d.getSeconds();
 
-	// set AM or PM
-	document.ampm.src=((hr<12)?dgam.src:dgpm.src);
+			// set AM or PM
+			document.ampm.src=((hr<12)?dgam.src:dgpm.src);
 
-	// adjust from 24hr clock
-	if(hr==0){hr=12;}
-	else if(hr>12){hr-=12;}
+			// adjust from 24hr clock
+			if(hr==0){hr=12;}
+			else if(hr>12){hr-=12;}
 
-	document.hr1.src = getSrc(hr,10);
-	document.hr2.src = getSrc(hr,1);
-	document.mn1.src = getSrc(mn,10);
-	document.mn2.src = getSrc(mn,1);
-	document.se1.src = getSrc(se,10);
-	document.se2.src = getSrc(se,1);
-}
+			document.hr1.src = getSrc(hr,10);
+			document.hr2.src = getSrc(hr,1);
+			document.mn1.src = getSrc(mn,10);
+			document.mn2.src = getSrc(mn,1);
+			document.se1.src = getSrc(se,10);
+			document.se2.src = getSrc(se,1);
+		}
 
-function getSrc(digit,index){
-	return dg[(Math.floor(digit/index)%10)].src;
-}
+		function getSrc(digit,index){
+			return dg[(Math.floor(digit/index)%10)].src;
+		}
 
-window.onload=function(){
-	dotime();
-	setInterval(dotime,1000);
-};
+		window.onload=function(){
+			dotime();
+			setInterval(dotime,1000);
+		};
 
       </script>
-    &nbsp;  
+      
 </p>
   </li>
   </fieldset>
-  <fieldset>
-	<div id="tablePrint"> </div>
-	<br>
-	<div id="lat"> Lat</div>
-	<div id="lon"> Lon</div>
+  	<fieldset>
+		<div id="tablePrint"> </div>
+	</fieldset>
+	<fieldset>
+		<div id="lat"> Lat</div>
+		<div id="lon"> Lon</div>
   </fieldset>
 </form>
  
